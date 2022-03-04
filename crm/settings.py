a8c142ff70ca6470eb50fed58cef086e7e9265c8
Isha Niroula <ishuniroula1999@gmail.com>
2022-03-04 10:00:41 +0545

    solved error

--- crm/settings.py
+++ crm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k5*##%)j7vifi=z5hb#von)o7!gzg%nvqy@vw5t7upn_vesf0t'
+# SECRET_KEY = 'k5*##%)j7vifi=z5hb#von)o7!gzg%nvqy@vw5t7upn_vesf0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
